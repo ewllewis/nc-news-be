60883573136c0780e668b09098a944c73202dd2a
ewllewis <ewl.lewis@gmail.com>
2025-04-30 14:23:44 +0100

    setup hosting

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,22 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || 'development'
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
+const config = {};
 
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
-} else { 
-    console.log(`Connected to ${process.env.PGDATABASE}`)
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-module.exports = db;
+// const db = new Pool();
+
+if (!process.env.PGDATABASE && !process.env.DATABSE_URL) {
+  throw new Error("No PGDATABASE or DATABASE_URL configured");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE}`);
+}
+
+module.exports = new Pool(config);
