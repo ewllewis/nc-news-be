b24a2755fc582f8b8015695b6a4db58006d2c6eb
ewllewis <ewl.lewis@gmail.com>
2025-05-01 10:53:11 +0100

    add GET /api/users/:username

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = new express.Router();
 
 //users.controller
-const { getUsers } = require("../controllers/users.controller");
+const {
+  getUsers,
+  getUserbyUsername,
+} = require("../controllers/users.controller");
 
 router.route("/").get(getUsers);
+router.route("/:username").get(getUserbyUsername);
 
 module.exports = router;
